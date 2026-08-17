4c3b404a6461c44507c7efbd7c38a3905273c2d0
hrushikesh-mahadik <hrushikeshm1905@gmail.com>
2026-08-17 17:38:44 +0530

    TreeSet

---
+++ collection/Set/TreeSet.java
@@ -0,0 +1,16 @@
+package collection.Set;
+import java.util.Set;
+
+
+public class TreeSet {
+    public static void main(String[] args) {
+         Set <Integer> v = new java.util.TreeSet<>();
+
+         v.add(20);
+         v.add(10);
+         v.add(30);
+
+         System.out.println(v);
+
+    }
+}
