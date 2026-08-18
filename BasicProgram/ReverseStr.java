699a2b23eb40646988a7fa8c0bf8d346675f33d4
hrushikesh-mahadik <hrushikeshm1905@gmail.com>
2026-08-18 19:49:57 +0530

    Reverse String

---
+++ BasicProgram/ReverseStr.java
@@ -0,0 +1,13 @@
+package BasicProgram;
+
+public class ReverseStr{
+    public static void main(String[] args) {
+        String  s = "hello";
+        String  r = "";
+
+        for(int i=s.length()-1;i>=0;i--){
+                r = r+s.charAt(i);     //
+        }
+        System.out.println(r);
+    }
+}
