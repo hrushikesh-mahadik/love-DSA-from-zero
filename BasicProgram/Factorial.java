811cdeba506d5883b640738b5742d7dce0321caa
hrushikesh-mahadik <hrushikeshm1905@gmail.com>
2026-08-19 23:17:22 +0530

    Basic Practice

---
+++ BasicProgram/Factorial.java
@@ -0,0 +1,12 @@
+package BasicProgram;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int a = 5;
+        int fact = 1;
+        for(int i =1 ; i<=5 ; i++){
+            fact = fact*i;
+        }
+        System.out.println(fact);
+    }    
+}
